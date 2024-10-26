422be4e0110f9cf4bf7a8fbf70f4ab2b691e2045
prathmeshbiradar <psbiradar948@gmail.com>
2024-10-26 11:12:21 +0530

    Adding main branch

---
+++ AddressBook/AddressBookMain.cs
@@ -0,0 +1,19 @@
+﻿using AddressBook.AddressBook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook.AddressBook
+{
+    public  class AddressBookMain
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to address book");
+           
+            
+        }
+    }
+}
