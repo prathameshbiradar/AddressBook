5f5f1f9fc6b199b1ee018e14bfe4dc03f5aa780d
prathmeshbiradar <psbiradar948@gmail.com>
2024-10-26 13:42:53 +0530

    Commit UC3

--- AddressBook/AddressBook.cs
+++ AddressBook/AddressBook.cs
@@ -8,7 +8,11 @@ namespace AddressBook.AddressBook
 {
     public class AddressBook
     {
-        public List<Contact> contacts = new List<Contact>();
+        public List<Contact> contacts;
+        public AddressBook()
+        {
+            contacts = new List<Contact>();
+        }
 
         public void addContact(Contact contact)
         {
@@ -21,6 +25,42 @@ namespace AddressBook.AddressBook
             {
                 Console.WriteLine($"name:{i.firstName} {i.lastName}, Address: {i.address}," +
                 $" City: {i.city}, State: {i.state}, Zip: {i.zip}, PhoneNumber: {i.phonenumber}, Email: {i.email} ");
+            }
+        }
+        public void editContact(String firstName, String lastName)
+        {
+            var contact = contacts.FirstOrDefault(c => c.firstName.Equals(firstName, StringComparison.OrdinalIgnoreCase) &&
+                         c.lastName.Equals(lastName, StringComparison.OrdinalIgnoreCase));
+
+            if (contact != null)
+            {
+                Console.WriteLine("Enter new Address: ");
+                String newAddress = Console.ReadLine();
+                contact.address = newAddress;
+
+                Console.WriteLine("Enter new City: ");
+                String newCity = Console.ReadLine();
+                contact.city = newCity;
+
+                Console.WriteLine("Enter new State: ");
+                String newState = Console.ReadLine();
+                contact.state = newState;
+
+                Console.WriteLine("Enter new Zip Code ");
+                String newZip = Console.ReadLine();
+                contact.zip = newZip;
+
+                Console.WriteLine("Enter new Phonenumber: ");
+                String newPhone = Console.ReadLine();
+                contact.phonenumber = newPhone;
+
+                Console.WriteLine("Enter new Email: ");
+                String newEmail = Console.ReadLine();
+                contact.email = newEmail;
+            }
+            else 
+            {
+                Console.WriteLine("Contact not found");
             }
         }
     }
--- AddressBook/AddressBookMain.cs
+++ AddressBook/AddressBookMain.cs
@@ -13,29 +13,71 @@ namespace AddressBook.AddressBook
         public static void Main(String[] args)
         {
             Console.WriteLine("Welcome to address book");
-
-            Contact newContact = new Contact();
             AddressBook addressbook = new AddressBook();
+            bool exit = false;
 
-            Console.WriteLine("Enter First Name:");
-            newContact.firstName = Console.ReadLine();
-            Console.WriteLine("Enter Last Name:");
-            newContact.lastName = Console.ReadLine();
-            Console.WriteLine("Enter Address: ");
-            newContact.address = Console.ReadLine();
-            Console.WriteLine("Enter City: ");
-            newContact.city = Console.ReadLine();
-            Console.WriteLine("Enter State: ");
-            newContact.state = Console.ReadLine();
-            Console.WriteLine("Enter Zip Code :");
-            newContact.zip = Console.ReadLine();
-            Console.WriteLine("Enter Phone Number");
-            newContact.phonenumber = Console.ReadLine();
-            Console.WriteLine("Enter Email: ");
-            newContact.email = Console.ReadLine();
+            while (!exit)
+            {
+                Console.WriteLine("\nMenu:");
+                Console.WriteLine("1. Add Contact");
+                Console.WriteLine("2. Edit Contact");
+                Console.WriteLine("3. Print Contact");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine("Choose an option: ");
+                String choice = Console.ReadLine();
 
-            addressbook.addContact(newContact);
-            addressbook.printContact();
+                switch (choice)
+                {
+                    case "1":
+                        Contact newContact = new Contact();
+                        Console.WriteLine("Enter First Name:");
+                        newContact.firstName = Console.ReadLine();
+                        Console.WriteLine("Enter Last Name:");
+                        newContact.lastName = Console.ReadLine();
+                        Console.WriteLine("Enter Address: ");
+                        newContact.address = Console.ReadLine();
+                        Console.WriteLine("Enter City: ");
+                        newContact.city = Console.ReadLine();
+                        Console.WriteLine("Enter State: ");
+                        newContact.state = Console.ReadLine();
+                        Console.WriteLine("Enter Zip Code :");
+                        newContact.zip = Console.ReadLine();
+                        Console.WriteLine("Enter Phone Number");
+                        newContact.phonenumber = Console.ReadLine();
+                        Console.WriteLine("Enter Email: ");
+                        newContact.email = Console.ReadLine();
+                        
+                        addressbook.addContact(newContact);
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Enter the first name of contact to edit:");
+                        String firstName = Console.ReadLine();
+                        Console.WriteLine("Enter Last name of contact to edit");
+                        String lastName = Console.ReadLine();
+                        addressbook.editContact(firstName, lastName);
+                        break;
+
+                    case "3":
+                        addressbook.printContact();
+                        break;
+
+                    case "4":
+                        exit = true;
+                        Console.WriteLine("Exiting the application..");
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Choice.Please Try Again");
+                        break;
+                }
+            }
+
+
+           
+            
+
+            
 
         }
     }
