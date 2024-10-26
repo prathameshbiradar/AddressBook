50f5a876fe86db56d3b14b112c6a288940480395
prathmeshbiradar <psbiradar948@gmail.com>
2024-10-26 11:15:37 +0530

    Commited UC1

--- AddressBook/AddressBookMain.cs
+++ AddressBook/AddressBookMain.cs
@@ -12,8 +12,25 @@ namespace AddressBook.AddressBook
         public static void Main(String[] args)
         {
             Console.WriteLine("Welcome to address book");
-           
-            
+
+            List<Contact> contacts = new List<Contact>();
+            Contact obj = new Contact()
+            {
+                firstName = "Prathmesh",
+                lastName = "Biradar",
+                address = "Karve nagar",
+                city = "Pune",
+                state = "Maharashtra",
+                zip = "422012",
+                phonenumber = "7843038332",
+                email = "psbiradar948@gmail.com"
+            };
+            contacts.Add(obj);
+            foreach (var i in contacts)
+            {
+                Console.WriteLine($"name:{i.firstName} {i.lastName}, Address: {i.address}, City: {i.city}, State: {i.state}, Zip: {i.zip}, PhoneNumber: {i.phonenumber}, Email: {i.email} ");
+            }
+
         }
     }
 }
---
+++ AddressBook/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook.AddressBook
+{
+      public class Contact
+      {
+            public String firstName { get; set; }
+            public String lastName { get; set; }
+            public String address { get; set; }
+            public String city { get; set; }
+            public String state { get; set; }
+            public String zip { get; set; }
+            public String phonenumber { get; set; }
+            public String email { get; set; }
+
+      }
+}
+
