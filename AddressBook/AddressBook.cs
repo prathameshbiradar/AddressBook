3765efa08c17cd8419e820e4985e4bc33f43a73e
prathmeshbiradar <psbiradar948@gmail.com>
2024-10-26 23:59:57 +0530

    Commited UC9

--- AddressBook/AddressBook.cs
+++ AddressBook/AddressBook.cs
@@ -9,9 +9,13 @@ namespace AddressBook.AddressBook
     public class AddressBook
     {
         public List<Contact> contacts;
+        public Dictionary<string, List<Contact>> contactsByCity;
+        public Dictionary<string, List<Contact>> contactsByState;
         public AddressBook()
         {
             contacts = new List<Contact>();
+            contactsByCity = new Dictionary<string, List<Contact>>(StringComparer.OrdinalIgnoreCase);
+            contactsByState = new Dictionary<string, List<Contact>>(StringComparer.OrdinalIgnoreCase);
         }
 
         public void addContact(Contact contact)
@@ -19,6 +23,8 @@ namespace AddressBook.AddressBook
             if (!contacts.Contains(contact))
             {
                 contacts.Add(contact);
+                AddToCityDictionary(contact);
+                AddToStateDictionary(contact);
                 Console.WriteLine("Contact added Successfully");
             }
             else
@@ -26,6 +32,23 @@ namespace AddressBook.AddressBook
                     Console.WriteLine("Duplicate contact found! Contact not added.");
             }
             
+        }
+        private void AddToCityDictionary(Contact contact)
+        {
+            if (!contactsByCity.ContainsKey(contact.city))
+            {
+                contactsByCity[contact.city] = new List<Contact>();
+            }
+            contactsByCity[contact.city].Add(contact);
+        }
+
+        private void AddToStateDictionary(Contact contact)
+        {
+            if (!contactsByState.ContainsKey(contact.state))
+            {
+                contactsByState[contact.state] = new List<Contact>();
+            }
+            contactsByState[contact.state].Add(contact);
         }
         public void printContact()
         {
@@ -89,6 +112,23 @@ namespace AddressBook.AddressBook
             {
                 Console.WriteLine("Contact not found.");
             }
+        }
+        public List<Contact> GetContactsByCity(string city)
+        {
+            if (contactsByCity.TryGetValue(city, out List<Contact> contactsInCity))
+            {
+                return contactsInCity;
+            }
+            return new List<Contact>(); 
+        }
+
+        public List<Contact> GetContactsByState(string state)
+        {
+            if (contactsByState.TryGetValue(state, out List<Contact> contactsInState))
+            {
+                return contactsInState;
+            }
+            return new List<Contact>(); 
         }
     }
 }
